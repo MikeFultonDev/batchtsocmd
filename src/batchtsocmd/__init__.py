f53f1bc5c5436d249e181655c1e8109101a53613
mike fulton <mikefultonpersonal@gmail.com>
2025-12-11 15:33:19 -0800

    bump release to 0.1.4

--- src/batchtsocmd/__init__.py
+++ src/batchtsocmd/__init__.py
@@ -2,7 +2,7 @@
 batchtsocmd - Execute TSO commands via IKJEFT1B with encoding conversion
 """
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 __author__ = "Mike Fulton"
 
 from .main import execute_tso_command, main
